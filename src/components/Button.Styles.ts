8d18b7aaa4193a26c0c040f9426bb76356a0f879
Lucas <lucasleitet@gmail.com>
2023-04-14 17:45:07 -0300

    Creating timer settings

---
+++ src/components/Button.Styles.ts
@@ -0,0 +1,33 @@
+import styled from "styled-components";
+
+export type ButtonVariant = "primary" | "secondary" | "danger" | "success";
+
+interface ButtonContainerProps{
+  variant: ButtonVariant;
+}
+
+const buttonVariant = {
+  primary: 'purple',
+  secondary: 'orange',
+  danger: 'red',
+  success: 'green'
+}
+
+export const ButtonContainer = styled.button <ButtonContainerProps>`
+width:100px;
+height:40px;
+
+background-color: ${props => props.theme.primary};
+color: ${props => props.theme.WHITE};
+border-radius: 4px;
+border: 0;
+margin: 8px;
+
+
+/* ${props => {
+  return `
+  background-color: ${buttonVariant[props.variant]}
+  `
+}  } */
+
+`;
---
+++ src/styles/themes/default.ts
@@ -0,0 +1,5 @@
+export const defaultTheme = {
+  WHITE: "#fff",
+  primary: "#8257e6",
+  secondary: "orange"
+}
