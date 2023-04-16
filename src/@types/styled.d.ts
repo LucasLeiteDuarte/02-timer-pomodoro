e7b2bcaa0c3a3fbe58f3cb5822e9492a8f81be70
Lucas <lucasleitet@gmail.com>
2023-04-16 14:07:14 -0300

    Declarate Theme

---
+++ src/@types/styled.d.ts
@@ -0,0 +1,10 @@
+// Aqui, SOMENTE codigo de definição de tipos de tipos do TS
+
+import 'styled-components';
+import{ defaultTheme } from '../styles/themes/default'
+
+type ThemeType = typeof defaultTheme;
+
+declare module 'styled-components' {
+  export interface defaultTheme extends ThemeType {}
+}
