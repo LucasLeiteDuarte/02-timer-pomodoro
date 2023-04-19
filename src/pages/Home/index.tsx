2b78a617b794582099fea2b579cf8031c5f1a5e6
Lucas <lucasleitet@gmail.com>
2023-04-18 22:25:00 -0300

    estrutra base

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,38 @@
+import { Play } from 'phosphor-react'
+import {
+  CountdownCOntainer,
+  FormContainer,
+  HomeContainer,
+  Separator,
+} from './styles'
+
+export function Home() {
+  return (
+    <HomeContainer>
+      <form action="">
+        <FormContainer>
+          <label htmlFor="task">Vou trabalhar em</label>
+          <input id="task" />
+
+          <label htmlFor="minutesAmount">durante</label>
+          <input type="number" id="minutesAmount" />
+
+          <span>minutos.</span>
+        </FormContainer>
+
+        <CountdownCOntainer>
+          <span>0</span>
+          <span>0</span>
+          <Separator>:</Separator>
+          <span>0</span>
+          <span>0</span>
+        </CountdownCOntainer>
+
+        <button type="submit">
+          <Play size={24} />
+          Começar
+        </button>
+      </form>
+    </HomeContainer>
+  )
+}
