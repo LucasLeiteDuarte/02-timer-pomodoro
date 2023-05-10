e6da69d56fc6d263129ff358bc0808423b8c2d92
Lucas <lucasleitet@gmail.com>
2023-05-10 16:48:20 -0300

    Estudos sobre contextos

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,23 +1,39 @@
-import { createContext, useContext } from 'react' // como se  fosse uma variável compartilhada entre dois componentes
-const CyclesContext = createContext({
-  activeCycle: 1,
-})
+import { createContext, useContext, useState } from 'react' // como se  fosse uma variável compartilhada entre dois componentes
+
+const CyclesContext = createContext({} as any)
 
 function NewCycleForm() {
-  const { activeCycle } = useContext(CyclesContext)
-  return <h1>NewCycleForm: {activeCycle}</h1>
+  const { activeCycle, setActiveCycle } = useContext(CyclesContext)
+
+  return (
+    <h1>
+      NewCycleForm: {activeCycle}
+      <button
+        onClick={() => {
+          setActiveCycle(2)
+        }}
+      >
+        Alterar o ciclo ativo
+      </button>
+    </h1>
+  )
 }
 
 function Countdown() {
   const { activeCycle } = useContext(CyclesContext)
+
   return <h1>Countdown: {activeCycle}</h1>
 }
 
 export function Home() {
+  const [activeCycle, setActiveCycle] = useState(0)
+
   return (
-    <div>
-      <NewCycleForm />
-      <Countdown />
-    </div>
+    <CyclesContext.Provider value={{ activeCycle, setActiveCycle }}>
+      <div>
+        <NewCycleForm />
+        <Countdown />
+      </div>
+    </CyclesContext.Provider>
   )
 }
